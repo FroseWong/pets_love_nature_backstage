164b64557427f45201c622f3e29cd1a368157d87
FroseWong <tibafrose@gmail.com>
2024-04-23 22:31:59 +0800

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,13 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="container-sm">100% wide until small breakpoint</div>
+      <div class="container-md">100% wide until medium breakpoint</div>
+      <div class="container-lg">100% wide until large breakpoint</div>
+      <div class="container-xl">100% wide until extra large breakpoint</div>
+      <div class="container-xxl">100% wide until extra extra large breakpoint</div>
+    </div>
   </div>
 </template>
 
