a610b12468a6c8d3fc245ee33840b1ad3b153a40
FroseWong <tibafrose@gmail.com>
2024-05-21 22:39:24 +0800

    測試報錯

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,7 +14,7 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld1 from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
